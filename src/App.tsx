b45e33c6345ee244118757e774cf49abb5f85646
crc1217dev <100065353+crc1217dev@users.noreply.github.com>
2023-06-16 21:12:47 +0900

    Add top header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
-
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
