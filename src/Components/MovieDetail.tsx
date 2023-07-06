0a0b4182ca41b07a309eeb7030691193e809b00b
crc1217dev <100065353+crc1217dev@users.noreply.github.com>
2023-07-06 19:31:58 +0900

    Fix detail design

--- src/Components/MovieDetail.tsx
+++ src/Components/MovieDetail.tsx
@@ -42,18 +42,13 @@ const Cover = styled(motion.div)<{ bgPhoto: string }>`
 `;
 
 const Title = styled.h3`
-  font-size: 28px;
-  top: -40px;
-  position: relative;
+  font-size: 32px;
+  font-weight: 500;
   color: ${(props) => props.theme.white.lighter};
-`;
-const DetailText = styled.p`
-  position: relative;
-  color: ${(props) => props.theme.white.lighter};
+  margin-bottom: 12px;
 `;
 
 const Contents = styled.div`
-  top: -30px;
   width: 100%;
   height: 100%;
   position: relative;
@@ -61,10 +56,19 @@ const Contents = styled.div`
   column-gap: 2em;
   grid-template-columns: minmax(0, 2fr) minmax(0, 1fr);
 `;
-const LeftContents = styled.div``;
-const RightContents = styled.div``;
-const OverView = styled(DetailText)`
-  background-color: ${(props) => props.theme.black.lighter};
+const LeftContents = styled.div`
+  padding: 12px;
+`;
+const RightContents = styled.div`
+  padding: 12px;
+`;
+
+const OverView = styled.div`
+  color: ${(props) => props.theme.white.lighter};
+  margin-bottom: 4px;
+`;
+const ContentTitle = styled.span`
+  color: rgba(147, 147, 147, 0.954);
 `;
 
 function MovieDetail({ selectedId }: IMovieDetailProps) {
@@ -86,13 +90,34 @@ function MovieDetail({ selectedId }: IMovieDetailProps) {
         {data && (
           <>
             <Cover bgPhoto={makeImagePath(data.backdrop_path, "w500")} />
-            <Title>{data.title}</Title>
             <Contents>
               <LeftContents>
+                <Title>{data.title}</Title>
                 <OverView>{data.overview}</OverView>
               </LeftContents>
               <RightContents>
-                <OverView>{data.overview}</OverView>
+                <OverView>
+                  <ContentTitle>Release date: </ContentTitle>
+                  {data.release_date}
+                </OverView>
+                <OverView>
+                  <ContentTitle>Runtime: </ContentTitle>
+                  {`${Math.floor(data.runtime / 60)} hours ${
+                    data.runtime % 60
+                  } min `}
+                </OverView>
+                <OverView>
+                  <ContentTitle>Genre: </ContentTitle>
+                  {data.genres.map((genre, index) => {
+                    return index === data.genres.length - 1
+                      ? genre.name
+                      : genre.name + ", ";
+                  })}
+                </OverView>
+                <OverView>
+                  <ContentTitle>status: </ContentTitle>
+                  {data.status}
+                </OverView>
               </RightContents>
             </Contents>
           </>
